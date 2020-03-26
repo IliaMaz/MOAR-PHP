3a5f4e339f41d37e06329e4c4519511a22f63284
IliaMaz <60601328+IliaMaz@users.noreply.github.com>
2020-03-26 10:26:23 +0100

    A few changes
    changes to cnstraints and data format.

--- Day 1/Exercises/search-movie.php
+++ Day 1/Exercises/search-movie.php
@@ -1,11 +1,18 @@
 <?php
-if (!empty($_POST)) {
+if (!empty($_POST) && $_POST['title']) {
     var_dump($_POST);
+    // * include DB_S_U_P_N
     require_once 'database.php';
+    // * Trim input
+    $search = trim($_POST['title']);
+    // * Connection
     $connect = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME, '3309');
-    $query = 'SELECT * FROM movies WHERE title LIKE ' . "'%$_POST[title]%'";
+    // * Query to use
+    $query = 'SELECT * FROM movies WHERE title LIKE ' . "'%$search%'";
     var_dump($query);
+    // * The actual query + connection and assignment of the result to a var
     $res_query = mysqli_query($connect, $query);
+    // * Show the result, while the results exists
     while ($row = mysqli_fetch_assoc($res_query)) {
         echo $row['title'] . '<br>' . $row['description'] . '<br>' . $row['realease_date'];
     }
--- Day 1/Exercises/search.php
+++ Day 1/Exercises/search.php
@@ -8,24 +8,31 @@
 </head>
 
 <body>
-    <input type="search" name="search" id="" placeholder="Search for movie">
+    <input type="search" name="search" placeholder="Search for movie">
     <div id="resDiv"></div>
     <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <script>
+        // * Listen to input, on keyup do function(e)
         $(function() {
             $('input[type="search"]').keyup(function(e) {
                 // e.preventDefault();
                 // console.log($('input[type="search"]').val());
-                const title = $('input[type="search"]').val();
+                // * Assign value of input to const
+                const title = $(this).val();
+                // * Do ajax post to search-movie.php, formatted as title: title
                 $.ajax({
                     type: 'POST',
                     url: 'search-movie.php',
                     data: {
                         title: title
                     },
+                    // ! In this case, using done() is better because there is no diff to what we do between success/error
+                    // ! If the results of err/res differ
+                    // * On success show res
                     success: function(res) {
                         $('#resDiv').html(res);
                     },
+                    // * On err show err
                     error: function(err) {
                         $('#resDiv').html(err);
                     }
